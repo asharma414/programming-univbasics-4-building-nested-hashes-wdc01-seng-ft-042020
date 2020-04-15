2bf518db53397988776f46515f5b6ea7ad2b279a
Abhinav Sharma <abhi.sharm.414@gmail.com>
2020-04-15 06:14:19 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,26 +34,5 @@ def monopoly_with_third_tier
 end
 
 def monopoly_with_fourth_tier
-  hash = { railroads: {} }
-  hash[:railroads] = {
-    :pieces => 4,
-    :rent_in_dollars => {},
-    :names => {
-      :reading_railroad => {},
-      :pennsylvania_railroad =>  {},
-      :b_and_o_railroad => {},
-      :shortline_railroad => {}
-    }
-  }
-  hash[:railroads][:rent_in_dollars] = {
-    :one_piece_owned => 25,
-    :two_pieces_owned => 50,
-    :three_pieces_owned => 75,
-    :four_pieces_owned => 100
-  }
-  hash[:railroads][:names][:reading_railroad] = { :mortgage_value => 100 }
-  hash[:railroads][:names][:pennsylvania_railroad] = { :mortgage_value => 200 }
-  hash[:railroads][:names][:b_and_o_railroad] = { :mortgage_value => 400 }
-  hash[:railroads[:names][:shortline_railroad] = { :mortgage_value => 800 }
-  hash
+
 end
